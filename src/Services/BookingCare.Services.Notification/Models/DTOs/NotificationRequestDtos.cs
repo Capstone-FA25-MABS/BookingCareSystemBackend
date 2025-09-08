449865cc6d05bd601449c244b2b73477938ff8a6
DevHuy44 <kieuvanhuy813@gmail.com>
2025-09-08 11:02:43 +0700

    [FEAT] Implement handle rabbitMQ and to do send email/phone

---
+++ src/Services/BookingCare.Services.Notification/Models/DTOs/NotificationRequestDtos.cs
@@ -0,0 +1,26 @@
+﻿using BookingCare.Shared.Common.Enums;
+
+namespace BookingCare.Services.Notification.Models.DTOs
+{
+    public class SendOtpRequest
+    {
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public OtpPurpose Purpose { get; set; } = OtpPurpose.REGISTER;
+        public string? DeviceId { get; set; } // Cho SMS
+    }
+
+    public class VerifyOtpRequest
+    {
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string Otp { get; set; } = "";
+        public OtpPurpose Purpose { get; set; } = OtpPurpose.REGISTER;
+    }
+
+    public class DeviceRegistrationRequest
+    {
+        public string DeviceName { get; set; } = "";
+        public string Token { get; set; } = "";
+    }
+}
