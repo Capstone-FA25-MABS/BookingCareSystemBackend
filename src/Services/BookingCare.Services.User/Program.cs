fb3565097645156cb8fa704f4c48b46701491867
DevHuy44 <kieuvanhuy813@gmail.com>
2025-09-15 22:11:35 +0700

    [FEAT] Implement write function call API

--- src/Services/BookingCare.Services.User/Program.cs
+++ src/Services/BookingCare.Services.User/Program.cs
@@ -1,5 +1,9 @@
 using BookingCare.Services.User.Data;
+using BookingCare.Services.User.Mappings;
+using BookingCare.Services.User.Repositories;
 using BookingCare.Services.User.Services;
+using BookingCare.Shared.Common.Extensions;
+using BookingCare.Shared.Common.Versioning;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,10 +29,34 @@ builder.WebHost.ConfigureKestrel(options =>
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Register AutoMapper
+builder.Services.AddAutoMapper(typeof(UserMappingProfile));
+
+// Register repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+// Register services
+builder.Services.AddScoped<IUserService, UserService>();
+
+// Add global exception handling
+builder.Services.AddGlobalExceptionHandling();
+
+// Add logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
 builder.Services.AddControllers();
 builder.Services.AddGrpc();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Add API versioning support
+builder.Services.AddApiVersioningSupport();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1.0", new() { Title = "BookingCare User API", Version = "v1.0" });
+});
 
 // Add CORS
 builder.Services.AddCors(options =>
@@ -56,13 +84,13 @@ using (var scope = app.Services.CreateScope())
     var context = scope.ServiceProvider.GetRequiredService<UserDbContext>();
     context.Database.EnsureCreated();
 }
-
+app.UseGlobalExceptionHandling();
 app.UseCors("AllowAll");
 app.UseRouting();
 app.MapControllers();
 
-// Configure the HTTP request pipeline.
-// app.MapGrpcService<UserGrpcService>(); // Temporarily commented out
+// Configure gRPC services
+app.MapGrpcService<UserGrpcService>();
 app.MapGet("/", () => "BookingCare User Service is running...");
 
 app.Run();
