303a40ea97971c2e09d6a56b1e8a2c2615883b55
DevHuy44 <kieuvanhuy813@gmail.com>
2025-09-16 14:02:08 +0700

    [FEAT] Create folder Constants

---
+++ src/Services/BookingCare.Services.Auth/Constants/AuthConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace BookingCare.Services.Auth.Constants
+{
+    public static class AuthConstants
+    {
+        public const string InvalidRequestData = "Invalid request data";
+        public const string ValidationError = "Validation error";
+    }
+}
