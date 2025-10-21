d3192dac55f65d1da2e2b11800a3d074dc54bba7
DevHuy44 <kieuvanhuy813@gmail.com>
2025-10-21 17:47:34 +0700

    [FEAT] Implement change relationship in RefundHistory

---
+++ src/Services/BookingCare.Services.Payment/Migrations/20251020165731_ChangeRelationShipRefundHistories.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingCare.Services.Payment.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeRelationShipRefundHistories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_refund_histories_payment_id",
+                table: "refund_histories");
+
+
+
+            migrationBuilder.CreateIndex(
+                name: "IX_refund_histories_payment_id",
+                table: "refund_histories",
+                column: "payment_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_refund_histories_payment_id",
+                table: "refund_histories");
+
+
+
+            migrationBuilder.CreateIndex(
+                name: "IX_refund_histories_payment_id",
+                table: "refund_histories",
+                column: "payment_id",
+                unique: true);
+        }
+    }
+}
