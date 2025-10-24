00abcabedc2f9bebe37e1fa6a887904df63cf8d8
DevHuy44 <kieuvanhuy813@gmail.com>
2025-10-24 15:54:27 +0700

    [FEAT] Implement add new migration MustChangePassword

--- src/Services/BookingCare.Services.Auth/Migrations/20251024083258_AddMustChangePassword.Designer.cs
+++ src/Services/BookingCare.Services.Auth/Migrations/20251024083258_AddMustChangePassword.Designer.cs
@@ -4,6 +4,7 @@ using BookingCare.Services.Auth.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingCare.Services.Auth.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251024083258_AddMustChangePassword")]
+    partial class AddMustChangePassword
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
