d969411dedbccc6145da1a6960a1dca02cb38e03
DevHuy44 <kieuvanhuy813@gmail.com>
2025-11-11 11:24:38 +0700

    [FEAT] Implement add new migration

---
+++ src/Services/BookingCare.Services.Auth/Migrations/20251110121849_Add2FAFieldsToAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingCare.Services.Auth.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add2FAFieldsToAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TwoFactorBackupCodes",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TwoFactorEnabledAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TwoFactorSecretKey",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TwoFactorBackupCodes",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFactorEnabledAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFactorSecretKey",
+                table: "AspNetUsers");
+        }
+    }
+}
